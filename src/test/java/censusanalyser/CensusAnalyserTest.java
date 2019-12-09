0c0da13d0e3e0b0f70d31fdeb2d8aef1eb94f574
akshaysb100 <akshaybavalekar100@gmail.com>
2019-12-09 11:40:23 +0530

    Add:Create function for calculate how much recode in state file

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -8,6 +8,7 @@ public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String INDIA_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -28,5 +29,17 @@ public class CensusAnalyserTest {
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
+    }
+
+    @Test
+    public void givenIndianStateCodeSCV_ShouldRetuenExtraCode() {
+
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        try {
+            int numOfRecords = censusAnalyser.loadIndiaSateCode(INDIA_CODE_CSV_FILE_PATH);
+            Assert.assertEquals(37,numOfRecords);
+        } catch (CensusAnalyserException e) {
+        }
+
     }
 }
