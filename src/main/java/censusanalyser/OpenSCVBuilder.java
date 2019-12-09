9acd858d59cb037a09491d82e4f4fd0297c0ebb5
akshaysb100 <akshaybavalekar100@gmail.com>
2019-12-09 12:59:23 +0530

    Refactor code for hanlde single responsibility principle

---
+++ src/main/java/censusanalyser/OpenSCVBuilder.java
@@ -0,0 +1,31 @@
+package censusanalyser;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
+
+public class OpenSCVBuilder <E>{
+
+
+    public Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+        try{
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            return csvToBean.iterator();
+        } catch (IllegalStateException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+        }
+    }
+
+    public int getCount(Iterator<E> eIterator) {
+        Iterable<E>  iterable = () -> eIterator;
+        int namOfEateries = (int) StreamSupport.stream(iterable.spliterator(),false).count();
+        return namOfEateries;
+    }
+}
