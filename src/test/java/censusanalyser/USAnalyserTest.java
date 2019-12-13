7b4992d1cad2ffc2efa81197d1dccb5d8a925690
akshaysb100 <akshaybavalekar100@gmail.com>
2019-12-13 11:44:35 +0530

    Refactor code for separated US test cases

--- src/test/java/censusanalyser/USAnalyserTest.java
+++ src/test/java/censusanalyser/USAnalyserTest.java
@@ -1,4 +1,65 @@
 package censusanalyser;
 
-public class USAnalayserTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class USAnalyserTest {
+
+    private static final String UC_CENSUS_CSV_FILE_PATH = "./src/test/resources/USCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_DELIMITER_CSV_FILE_PATH = "./src/test/resources/delimiterWrong.csv";
+    private static final String WRONG_HEADER_CSV_FILE_PATH = "./src/test/resources/headerWrong.csv";
+
+    CensusAnalyser censusAnalyser = new CensusAnalyser();
+
+    @Test
+    public void givenUSCensusCSVFile_ReturnsCorrectRecords() {
+        try {
+
+            int numOfRecords = censusAnalyser.loadCSVCensusData(CensusAnalyser.Country.USA,UC_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(51, numOfRecords);
+        } catch (CensusAnalyserException e) {
+        }
+    }
+
+    @Test
+    public void givenUSCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadCSVCensusData(CensusAnalyser.Country.USA, WRONG_CSV_FILE_PATH);
+            Assert.assertEquals(51, numOfRecords);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenUSCSVFile_WhenHeaderNotAvailable_ThrowsException() {
+        try {
+            int numOfRecords = censusAnalyser.loadCSVCensusData(CensusAnalyser.Country.USA, WRONG_HEADER_CSV_FILE_PATH);
+            Assert.assertEquals(51, numOfRecords);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.SOME_OTHER_FILE_ERROR, e.type);
+        }
+    }
+
+    @Test
+    public void givenUSCSVFile_WhenPassEmptyFile_ThrowsException() {
+        try {
+            int numOfRecords = censusAnalyser.loadCSVCensusData(CensusAnalyser.Country.USA, "");
+            Assert.assertEquals(51, numOfRecords);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.SOME_OTHER_FILE_ERROR, e.type);
+        }
+    }
+
+    @Test
+    public void givenUSCensusData_WithWrongDelimiter_ShouldThrowException() {
+        try {
+            int numOfRecords = censusAnalyser.loadCSVCensusData(CensusAnalyser.Country.INDIA, WRONG_DELIMITER_CSV_FILE_PATH);
+            Assert.assertEquals(51, numOfRecords);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.SOME_OTHER_FILE_ERROR, e.type);
+        }
+    }
 }
